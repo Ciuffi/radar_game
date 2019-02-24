4ab2f54eafef95fcdfeb760e2e1251a63b328c72
Giulio Rossi <ciuffi9@mac.com>
2019-02-23 23:29:23 -0800

    Initial commit with movement

---
+++ Assets/movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    private bool jumpable = true;
+
+    public float drag;
+
+    public float jumpspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        jumpable = true;
+    }
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+
+        Vector2 vel = new Vector2 (moveHorizontal * speed, rb2d.velocity.y);
+        vel.x *= 1.0f - drag;
+        vel.y *= 1.0f - drag;
+
+        rb2d.velocity = vel;
+        if (Input.GetKeyDown("space"))
+        {
+            rb2d.AddForce(new Vector2(0, jumpspeed), ForceMode2D.Impulse);
+            jumpable = false;
+        }
+    }
+}
