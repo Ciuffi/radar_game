43571af84eab317cea4b38ad85144cb938563401
Andy Kim <w9h0b@ugrad.cs.ubc.ca>
2019-02-28 00:22:54 -0800

    Partial implementation of jump

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -2,38 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum PlayerState { Idle, Jumping };
+
 public class movement : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     public float speed;
     private bool jumpable = true;
+    private float jumpTime = 0.5f;
+    
 
     public float drag;
 
-    public float jumpspeed;
+    [Tooltip("Kenrick is chimpin out")]
+    [SerializeField]
+    private float jumpspeed;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         rb2d = GetComponent<Rigidbody2D> ();
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
+    private void OnCollisionEnter2D(Collision2D collision){
         jumpable = true;
+        jumpTime = 0.5f;
     }
-    
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
 
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+    
         Vector2 vel = new Vector2 (moveHorizontal * speed, rb2d.velocity.y);
         vel.x *= 1.0f - drag;
         vel.y *= 1.0f - drag;
+        
+        
 
         rb2d.velocity = vel;
-        if (Input.GetKeyDown("space"))
-        {
-            rb2d.AddForce(new Vector2(0, jumpspeed), ForceMode2D.Impulse);
+        // Farther away from the ground you are, the less push it has
+        // To a limit
+        // TODO: Once character is falling, they should not be able to jump again. Current implementation is literally a jetpack
+        if (Input.GetKey(KeyCode.Space)){
+            if (jumpable){
+                rb2d.AddForce(new Vector2(0, jumpspeed), ForceMode2D.Impulse);
+            } else {
+                rb2d.AddForce(new Vector2(0, jumpspeed * Mathf.Max(0f,jumpTime)/ 0.5f), ForceMode2D.Impulse);
+            }
+            jumpTime -= Time.deltaTime;
             jumpable = false;
         }
     }
