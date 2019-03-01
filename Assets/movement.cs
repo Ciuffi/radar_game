1bb36edd26619ae5c6cd60ecd12208f0f4f92676
Giulio Rossi <ciuffi9@mac.com>
2019-02-28 16:58:43 -0800

    Fix some stuff, rename some variables u know how it is

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -3,45 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 enum PlayerState { Idle, Jumping, Falling };
-enum moveState { Idle, Moving};
+enum MoveState { Idle, Moving};
 public class movement : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
-    public float speed;
-    private float jumpTime = 0.5f;
+    //Radar Variables
+    public float HorizontalSpeed;
+    public float HorizontalDrag;
+    public float JumpForce;
+
+    //No Radar variables
     private PlayerState myState;
-    
-    private moveState myMoveState;
+    private MoveState myMoveState;
+    private Rigidbody2D rb2d;
+    private float jumpTimer;
 
-    public float drag;
-
-    [Tooltip("Kenrick is chimpin out")]
-    [SerializeField]
-    private float jumpspeed;
     // Start is called before the first frame update
     void Start(){
         rb2d = GetComponent<Rigidbody2D> ();
+        myState = PlayerState.Idle;
+        myMoveState = MoveState.Idle;
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
-        jumpTime = 0.5f;
+        jumpTimer = 0.4f;
         myState = PlayerState.Idle;
-        myMoveState = moveState.Idle;
     }
 
     void FixedUpdate(){
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         Vector2 vel;
-        if(rb2d.velocity.x == 0 ){
-            myMoveState = moveState.Idle;
+        if((rb2d.velocity.x <= 0.01 && rb2d.velocity.x > 0) || 
+            rb2d.velocity.x >= -0.01 && rb2d.velocity.x < 0){
+            myMoveState = MoveState.Idle;
         }
         if (moveHorizontal > 0 || moveHorizontal < 0){
-            myMoveState = moveState.Moving;
-            vel = new Vector2 (moveHorizontal * speed, rb2d.velocity.y);
-        }else if (myMoveState == moveState.Moving) {
-            vel = new Vector2 (rb2d.velocity.x *  1.0f - drag, rb2d.velocity.y);
+            myMoveState = MoveState.Moving;
+            vel = new Vector2 (moveHorizontal * HorizontalSpeed, rb2d.velocity.y);
+        }else if (myMoveState == MoveState.Moving) {
+            vel = new Vector2 (rb2d.velocity.x *  (1.0f - HorizontalDrag), rb2d.velocity.y);
         }else{
-            vel = new Vector2 (moveHorizontal * speed, rb2d.velocity.y);
+            vel = new Vector2 (moveHorizontal * HorizontalSpeed, rb2d.velocity.y);
         }
         
         rb2d.velocity = vel;
@@ -49,12 +50,12 @@ public class movement : MonoBehaviour
         // To a limit
         if (Input.GetKeyDown(KeyCode.Space) && myState == PlayerState.Idle){
             myState = PlayerState.Jumping;
-            rb2d.AddForce(new Vector2(0, jumpspeed), ForceMode2D.Impulse);
-            jumpTime -= Time.deltaTime;
+            rb2d.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            jumpTimer -= Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.Space) && myState == PlayerState.Jumping){
-            rb2d.AddForce(new Vector2(0, jumpspeed * Mathf.Max(0f, jumpTime) / 0.5f), ForceMode2D.Impulse);
-            jumpTime -= Time.deltaTime;
+            rb2d.AddForce(new Vector2(0, JumpForce * Mathf.Max(0f, jumpTimer) / 0.5f), ForceMode2D.Impulse);
+            jumpTimer -= Time.deltaTime;
         }
         if (Input.GetKeyUp(KeyCode.Space) && myState == PlayerState.Jumping) {
             myState = PlayerState.Falling;
